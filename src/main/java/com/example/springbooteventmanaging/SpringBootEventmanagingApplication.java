6e2fbdf29f7cb8c9b1d77144b62a27a95c5978be
Francis Rodgers <benjamin-zipperer@web.de>
2019-03-20 09:21:48 +0100

    create master branch

---
+++ src/main/java/com/example/springbooteventmanaging/SpringBootEventmanagingApplication.java
@@ -0,0 +1,25 @@
+package com.example.springbooteventmanaging;
+import org.springframework.boot.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class SpringBootEventmanagingApplication {
+
+	@RequestMapping("/testaufruf")
+	String home() {
+		return "Dies ist eine Testzeile!\nScheint alles zu funktionieren";
+	}
+	public static void main(String[] args)
+	{
+		SpringApplication.run
+				(SpringBootEventmanagingApplication.class, args);
+	}
+
+}
